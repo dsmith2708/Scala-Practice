b2896471fc5981445e81e13901d95951aa97122f
Daniel Smith <danielsmith.2796@googlemail.com>
2019-07-02 16:03:32 +0100

    project new uses class to display a persons name and age

--- Beginnings/src/main/scala/HelloWorld.scala
+++ Beginnings/src/main/scala/HelloWorld.scala
@@ -1,5 +1,11 @@
 class HelloWorld(var name: String, var age: Int)
 
 object Main extends App {
+
+  val newPerson = new HelloWorld("Daniel", 22);
+
   println("Hello World");
+  println("The name of newPerson is:" + newPerson.name);
+  println("The age of newPerson is:" + newPerson.age);
+
 }
